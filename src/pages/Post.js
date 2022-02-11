90a4620d0bfedc8f82970cfdb024e34df59b8143
ZuguangTong <810732627@qq.com>
2022-02-12 10:21:19 +1100

    add comment

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -6,6 +6,7 @@ function Post(props) {
     let { id } = useParams()
     const [postObject, setPostObject] = useState({})
     const [comments, setComments] = useState([])
+    const [newComment, setNewComment] = useState("")
     useEffect(() => {
         axios.get(`http://localhost:3301/posts/byId/${id}`).then((response) => {
             console.log(response)
@@ -15,7 +16,15 @@ function Post(props) {
         axios.get(`http://localhost:3301/comments/${id}`).then((response) => {
             setComments(response.data)
         })
-    },[])
+    }, [])
+
+    const addComment = () => {
+        axios.post(`http://localhost:3301/comments`, { commentBody: newComment, PostId: id }).then((res) => {
+            const commentToAdd = { commentBody: newComment }
+            setComments([...comments, commentToAdd])
+            setNewComment("")
+        })
+    }
     return (
         <div>
             <div className="leftSide">
@@ -25,12 +34,12 @@ function Post(props) {
             </div>
             <div className="rightSide">
                 <div className="addCommentContainer">
-                    <input type="text" placeholder="Comment:" />
-                    <button>Add Comment</button>
+                    <input type="text" value={newComment} placeholder="Comment:" onChange={(e) => { setNewComment(e.target.value) }} />
+                    <button onClick={addComment}>Add Comment</button>
                 </div>
                 <div className="listOfComment">
                     {
-                        comments.map((comment,key)=>{
+                        comments.map((comment, key) => {
                             return <div className="comment" key={key}>{comment.commentBody}</div>
                         })
                     }
